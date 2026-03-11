b383d5ca658e90dc96cc1084862859ae31b4e954
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2026-03-10 23:22:28 -0300

    Fixing date picker for the last time

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,20 +3,33 @@ import flatpickr from "flatpickr"
 
 export default class extends Controller {
   connect() {
-    if (this.element._flatpickr) {
-      this.element._flatpickr.destroy()
-    }
-
-    this.picker = flatpickr(this.element, {
-      dateFormat: "Y-m-d",
-    })
+    this._initPicker()
 
     this._beforeCache = () => this.picker?.destroy()
     document.addEventListener("turbo:before-cache", this._beforeCache)
+
+    this._onVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
+        this._initPicker()
+      }
+    }
+    document.addEventListener("visibilitychange", this._onVisibilityChange)
   }
 
   disconnect() {
     document.removeEventListener("turbo:before-cache", this._beforeCache)
+    document.removeEventListener("visibilitychange", this._onVisibilityChange)
     this.picker?.destroy()
+  }
+
+  _initPicker() {
+    const currentValue = this.element.value
+    if (this.element._flatpickr) {
+      this.element._flatpickr.destroy()
+    }
+    this.picker = flatpickr(this.element, {
+      dateFormat: "Y-m-d",
+      defaultDate: currentValue || null,
+    })
   }
 }
